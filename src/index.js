6c739ca2574dd033508527bbdc43ceb385e35291
Greg <gggolub1@gmail.com>
2020-04-18 10:15:33 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Cards from './cards/Cards'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Cards />
   </React.StrictMode>,
   document.getElementById('root')
 );
